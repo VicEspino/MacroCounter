07b9ab41285f3637820c578342c434b6c8b835d4
VicEspino <taxgype1@gmail.com>
2020-12-11 01:42:21 -0600

    Global Chat Screen and items related, added.

---
+++ app/src/main/java/com/example/macrocounter/FragmentGlobalChat.java
@@ -0,0 +1,28 @@
+package com.example.macrocounter;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentGlobalChat extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_global_chat, container, false);
+
+        return root;
+    }
+}
