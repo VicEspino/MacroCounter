a7c53a19edac13a6047ff7b824f0133b60f250b8
VicEspino <taxgype1@gmail.com>
2020-12-15 20:06:39 -0600

    Screen changing works properly.

--- app/src/main/java/com/example/macrocounter/UI/GlobalChat/FragmentGlobalChat.java
+++ app/src/main/java/com/example/macrocounter/UI/GlobalChat/FragmentGlobalChat.java
@@ -1,18 +1,36 @@
 package com.example.macrocounter.UI.GlobalChat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.macrocounter.R;
+import com.example.macrocounter.UI.UserLoged.UserLogedActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class FragmentGlobalChat extends AppCompatActivity {
+
+    private String userName;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_global_chat);
+        Intent intent = getIntent();
+        this.userName = intent.getStringExtra("UserName");
 
+        FloatingActionButton fabChangeHistory = findViewById(R.id.fab_history);
+        fabChangeHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(FragmentGlobalChat.this, UserLogedActivity.class);
+                myIntent.putExtra("UserName", userName);
+                startActivity(myIntent);
+                finish();
+            }
+        });
     }
 
 }
